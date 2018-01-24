9d607851da028e99761cf0cfadc0a43022430249
mingzhen_yang <mingzhen.yang@b-and-qchina.com>
2018-01-24 10:11:13 +0800

    上传一个  MyPerson01 类，

---
+++ test/test/MyPerson01.h
@@ -0,0 +1,13 @@
+//
+//  MyPerson01.h
+//  test
+//
+//  Created by user on 2018/1/24.
+//  Copyright © 2018年 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyPerson01 : NSObject
+
+@end
---
+++ test/test/MyPerson01.m
@@ -0,0 +1,13 @@
+//
+//  MyPerson01.m
+//  test
+//
+//  Created by user on 2018/1/24.
+//  Copyright © 2018年 user. All rights reserved.
+//
+
+#import "MyPerson01.h"
+
+@implementation MyPerson01
+
+@end
