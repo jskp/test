e1f5c34d3ee909129cb7f251439789d7a9d7756f
mingzhen_yang <mingzhen.yang@b-and-qchina.com>
2018-01-24 09:51:05 +0800

    上传新的版本 1.0，

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by user on 2018/1/24.
+//  Copyright © 2018年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
