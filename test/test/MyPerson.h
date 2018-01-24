9e17919a365788e1eaddd802b09230de40dfcd10
mingzhen_yang <mingzhen.yang@b-and-qchina.com>
2018-01-24 10:10:24 +0800

    上传一个Person类，

---
+++ test/test/MyPerson.h
@@ -0,0 +1,13 @@
+//
+//  MyPerson.h
+//  test
+//
+//  Created by user on 2018/1/24.
+//  Copyright © 2018年 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyPerson : NSObject
+
+@end
---
+++ test/test/MyPerson.m
@@ -0,0 +1,13 @@
+//
+//  MyPerson.m
+//  test
+//
+//  Created by user on 2018/1/24.
+//  Copyright © 2018年 user. All rights reserved.
+//
+
+#import "MyPerson.h"
+
+@implementation MyPerson
+
+@end
